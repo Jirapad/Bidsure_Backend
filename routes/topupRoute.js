af502905f146d8f81d865d44c7d563e0d17668b1
Jirapad Sanguanpan <jirapadschool@gmail.com>
2024-04-08 07:47:28 +0700

    add express raw in webhook route

--- routes/topupRoute.js
+++ routes/topupRoute.js
@@ -1,10 +1,11 @@
 const { checkOut, getOrderId, webhook } = require('../controllers/topupController')
 const { authentication } = require('../middlewares/authentication')
+const express = require('express')
 
 const router = require('express').Router()
 
 router.route('/checkout').post(authentication,checkOut)
 router.route('/getorder/:id').get(getOrderId)
-router.route('/webhook').post(webhook)
+router.route('/webhook').post(express.raw({type:'application/json'}),webhook)
 
 module.exports = router
